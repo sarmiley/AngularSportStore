79971260880048f9183d85e41ac2c84e598c1596
Matt Liu <matt.liu@tpisoftware.com>
2022-03-24 14:57:52 +0800

    producy 分頁功能

--- src/app/store/store/store.component.ts
+++ src/app/store/store/store.component.ts
@@ -8,19 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./store.component.css'],
 })
 export class StoreComponent {
-  public selectCateory: string | null = null;
+  selectCateory: string | null = null;
+  productsPerPage = 4;
+  selectPage = 1;
 
   constructor(private repo: ProductRepository) {}
 
   get products(): Product[] {
-    return this.repo.getProducts(this.selectCateory);
+    let pageIndex = (this.selectPage - 1) * this.productsPerPage;
+    return this.repo
+      .getProducts(this.selectCateory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
     return this.repo.getCategories();
   }
 
-  changeCategory(newCategory: string | null = null) {
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repo.getProducts(this.selectCateory).length / this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  changeCategory(newCategory: string | null = null): void {
     this.selectCateory = newCategory;
+  }
+
+  changePage(newPage: number): void {
+    this.selectPage = newPage;
+  }
+
+  changePageSize(newSize: any): void {
+    console.log(newSize);
+
+    this.productsPerPage = newSize ?? 3;
+    this.changePage(1);
   }
 }
